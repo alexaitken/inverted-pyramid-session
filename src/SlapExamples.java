0b69ae81bd0f62d74054b0f732be7abb7b6e6ba1
Alex Aitken <alex@alexaitken.com>
2013-10-03 14:27:43 -0400

    Fix spelling.

--- src/SlapExamples.java
+++ src/SlapExamples.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class SlapExamples {
 
-	
+
 	public void addPeople(List<Person> people) {
 		for (Person person : people) {
 			placeInNameRegistry(person);
@@ -12,7 +12,7 @@ public class SlapExamples {
 			sendWelcomeEmail(person);
 		}
 	}
-	
+
 	public List<Person> addPeopleWithFailures(List<Person> people) {
 		List<Person> failedPeople = new ArrayList<>();
 		for (Person person : people) {
@@ -26,56 +26,56 @@ public class SlapExamples {
 		}
 		return failedPeople;
 	}
-	
-	
+
+
 
 	public Person parseName(String rawName) {
 		String[] namePieces = rawName.split(" ");
 		String firstName, lastName;
-		
+
 		if (namePieces.length < 2)
 			throw new IllegalArgumentException("We must have 2 names");
-		
+
 		firstName = namePieces[0];
 		lastName = namePieces[namePieces.length - 1];
-		
+
 		return new Person(firstName, lastName);
 	}
-	
-	
+
+
 	public void removePerson(Person person) {
 		removeFromNameRegistry(person);
 		notifyPhoneSystem();
-		sendFairwellEmail(person);
+		sendFarewellEmail(person);
 	}
-	
+
 	private void sendFairwellEmail(Person person) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void removeFromNameRegistry(Person person) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void sendWelcomeEmail(Person person) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void updatePhoneSystem() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void placeInNameRegistry(Person person) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	private void notifyPhoneSystem() {
 		// TODO Auto-generated method stub
-		
+
 	}
 }

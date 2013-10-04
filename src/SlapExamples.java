a2c83765f7b8473375f18ef5e86853b0a0ec3e89
Alex Aitken <alex@alexaitken.com>
2013-10-03 21:13:49 -0400

    Added examples and updated formatting.

--- src/SlapExamples.java
+++ src/SlapExamples.java
@@ -1,81 +1,91 @@
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class SlapExamples {
 
+  public void addPeople(List<Person> people) {
+    for (Person person : people) {
+      placeInNameRegistry(person);
+      updatePhoneSystem();
+      sendWelcomeEmail(person);
+    }
+  }
 
-	public void addPeople(List<Person> people) {
-		for (Person person : people) {
-			placeInNameRegistry(person);
-			updatePhoneSystem();
-			sendWelcomeEmail(person);
-		}
-	}
+  public List<Person> addPeopleWithFailures(List<Person> people) {
+    List<Person> failedPeople = new ArrayList<>();
+    for (Person person : people) {
+      try {
+        placeInNameRegistry(person);
+        notifyPhoneSystem();
+        sendWelcomeEmail(person);
+      } catch (Exception e) {
+        failedPeople.add(person);
+      }
+    }
+    return failedPeople;
+  }
+  
+  public boolean addPersonWithFailures(Person person) {
+    try {
+      placeInNameRegistry(person);
+      notifyPhoneSystem();
+      sendWelcomeEmail(person);
+      return true;
+    } catch (Exception e) {
+      return false;
+    }
+  }
+  
+  
+  public boolean addPersonWithFailures2(Person person) {
+    try {
+      addPerson(person);
+      return true;
+    } catch (Exception e) {
+      return false;
+    }
+  }
+  
+  public void addPerson(Person person) {
+    placeInNameRegistry(person);
+    notifyPhoneSystem();
+    sendWelcomeEmail(person);
+  }
 
-	public List<Person> addPeopleWithFailures(List<Person> people) {
-		List<Person> failedPeople = new ArrayList<>();
-		for (Person person : people) {
-			try {
-				placeInNameRegistry(person);
-				notifyPhoneSystem();
-				sendWelcomeEmail(person);
-			} catch (Exception e) {
-				failedPeople.add(person);
-			}
-		}
-		return failedPeople;
-	}
+  public Person parseName(String rawName) {
+    String[] namePieces = rawName.split(" ");
+    String firstName, lastName;
 
+    if (namePieces.length < 2)
+      throw new IllegalArgumentException("We must have 2 names");
 
+    firstName = namePieces[0];
+    lastName = namePieces[namePieces.length - 1];
 
-	public Person parseName(String rawName) {
-		String[] namePieces = rawName.split(" ");
-		String firstName, lastName;
+    return new Person(firstName, lastName);
+  }
 
-		if (namePieces.length < 2)
-			throw new IllegalArgumentException("We must have 2 names");
+  public void removePerson(Person person) {
+    removeFromNameRegistry(person);
+    notifyPhoneSystem();
+    sendFarewellEmail(person);
+  }
 
-		firstName = namePieces[0];
-		lastName = namePieces[namePieces.length - 1];
+  private void sendFarewellEmail(Person person) {
+  }
 
-		return new Person(firstName, lastName);
-	}
+  private void removeFromNameRegistry(Person person) {
+  }
 
+  private void sendWelcomeEmail(Person person) {
+  }
 
-	public void removePerson(Person person) {
-		removeFromNameRegistry(person);
-		notifyPhoneSystem();
-		sendFarewellEmail(person);
-	}
+  private void updatePhoneSystem() {
+  }
 
-	private void sendFairwellEmail(Person person) {
-		// TODO Auto-generated method stub
+  private void placeInNameRegistry(Person person) {
+  }
 
-	}
-
-	private void removeFromNameRegistry(Person person) {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void sendWelcomeEmail(Person person) {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void updatePhoneSystem() {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void placeInNameRegistry(Person person) {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void notifyPhoneSystem() {
-		// TODO Auto-generated method stub
-
-	}
+  private void notifyPhoneSystem() {
+  }
 }
--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,79 +1,92 @@
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("unused")
 public class StringCalculator {
 
-	public int add(String numbers) {
-		List<Integer> values = convertToInteger(parseValues(numbers));
-		failWhenContainsNegatives(values);
-		return sumOf(values);
-	}
-	
-	private String[] parseValues(String numbers) {
-		if (numbers.isEmpty()) {
-			return new String[] {};
-		}
-		if (containsCustomDelimiters(numbers)) {
-			return parseCustomDelimitedValues(numbers);
-		}
-		return parseStandardDelimiters(numbers);
-	}
-	
-	private List<Integer> convertToInteger(String[] numbers) {
-		List<Integer> result = new ArrayList<>();
-		for (String number : numbers) {
-			result.add(toInteger(number));
-		}
-		return result;
-	}
-	
-	private void failWhenContainsNegatives(List<Integer> values) {
-		List<Integer> negatives = new ArrayList<>();
-		for (Integer value : values) {
-			if (value < 0)
-				negatives.add(value);
-		}
-		if (!negatives.isEmpty()) {
-			throw new RuntimeException("Error: negatives not allowed " + negatives);
-		}
-	}
-	
-	private int sumOf(List<Integer> values) {
-		int result = 0;
-		for (Integer value : values) {
-			result += value;
-		}
-		return result;
-	}
-	
-	private Integer toInteger(String number) {
-		Integer value = Integer.parseInt(number);
-		return value <= 1000 ? value : 0;
-	}
+  public int add(String numbers) {
+    List<Integer> values = convertToInteger(parseValues(numbers));
+    failWhenContainsNegatives(values);
+    return sumOf(values);
+  }
 
-	private boolean containsCustomDelimiters(String numbers) {
-		return numbers.startsWith("//");
-	}
+  private String[] parseValues(String numbers) {
+    if (numbers.isEmpty()) {
+      return new String[] {};
+    }
+    if (containsCustomDelimiters(numbers)) {
+      return parseCustomDelimitedValues(numbers);
+    }
+    return parseStandardDelimiters(numbers);
+  }
 
-	private String[] parseCustomDelimitedValues(String numbers) { /* this is a good method for slap */
-		int newlineIndex = numbers.indexOf('\n');
-		String rawCustomDelimiters = numbers.substring(2, newlineIndex);
-		String numberList = numbers.substring(newlineIndex + 1);
-		for (String customDelimiter : parseCustomDelimiters(rawCustomDelimiters)) {
-			numberList = numberList.replaceAll(quoteRegularExpression(customDelimiter), ",");
-		}
-		return numberList.split(",");
-	}
+  private List<Integer> convertToInteger(String[] numbers) {
+    List<Integer> result = new ArrayList<>();
+    for (String number : numbers) {
+      result.add(toInteger(number));
+    }
+    return result;
+  }
 
-	private String[] parseCustomDelimiters(String rawCustomDelimiters) {
-		return rawCustomDelimiters.replaceAll("\\[", "").split("\\]");
-	}
+  private List<Integer> convertToIntegerNonSlap(String[] numbers) {
+    List<Integer> result = new ArrayList<>();
+    for (String number : numbers) {
+      Integer value = Integer.parseInt(number);
+      result.add(value <= 1000 ? value : 0);
+    }
+    return result;
+  }
 
-	private String quoteRegularExpression(String customSeparator) {
-		return "\\Q" + customSeparator + "\\E";
-	}
+  private void failWhenContainsNegatives(List<Integer> values) {
+    List<Integer> negatives = new ArrayList<>();
+    for (Integer value : values) {
+      if (value < 0)
+        negatives.add(value);
+    }
+    if (!negatives.isEmpty()) {
+      throw new RuntimeException("Error: negatives not allowed " + negatives);
+    }
+  }
 
-	private String[] parseStandardDelimiters(String numbers) {
-		return numbers.split("[,\n]");
-	}
+  private int sumOf(List<Integer> values) {
+    int result = 0;
+    for (Integer value : values) {
+      result += value;
+    }
+    return result;
+  }
+
+  private Integer toInteger(String number) {
+    Integer value = Integer.parseInt(number);
+    return value <= 1000 ? value : 0;
+  }
+
+  private boolean containsCustomDelimiters(String numbers) {
+    return numbers.startsWith("//");
+  }
+
+  private String[] parseCustomDelimitedValues(String numbers) { /*
+                                                                 * this is a good method for slap
+                                                                 */
+    int newlineIndex = numbers.indexOf('\n');
+    String rawCustomDelimiters = numbers.substring(2, newlineIndex);
+    String numberList = numbers.substring(newlineIndex + 1);
+    for (String customDelimiter : parseCustomDelimiters(rawCustomDelimiters)) {
+      numberList = numberList.replaceAll(
+          quoteRegularExpression(customDelimiter), ",");
+    }
+    return numberList.split(",");
+  }
+
+  private String[] parseCustomDelimiters(String rawCustomDelimiters) {
+    return rawCustomDelimiters.replaceAll("\\[", "").split("\\]");
+  }
+
+  private String quoteRegularExpression(String customSeparator) {
+    return "\\Q" + customSeparator + "\\E";
+  }
+
+  private String[] parseStandardDelimiters(String numbers) {
+    return numbers.split("[,\n]");
+  }
 }

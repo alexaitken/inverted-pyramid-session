71561afe067a3b146914f5cae73784d070848227
Alex Aitken <alex@alexaitken.com>
2013-10-06 19:45:11 -0400

    Change formatting.

--- src/SlapExamples.java
+++ src/SlapExamples.java
@@ -3,89 +3,90 @@ import java.util.List;
 
 public class SlapExamples {
 
-  public void addPeople(List<Person> people) {
-    for (Person person : people) {
-      placeInNameRegistry(person);
-      updatePhoneSystem();
-      sendWelcomeEmail(person);
+    public void addPeople(List<Person> people) {
+        for (Person person : people) {
+            placeInNameRegistry(person);
+            updatePhoneSystem();
+            sendWelcomeEmail(person);
+        }
     }
-  }
 
-  public List<Person> addPeopleWithFailures(List<Person> people) {
-    List<Person> failedPeople = new ArrayList<>();
-    for (Person person : people) {
-      try {
+    public List<Person> addPeopleWithFailures(List<Person> people) {
+        List<Person> failedPeople = new ArrayList<>();
+        
+        for (Person person : people) {
+            try {
+                placeInNameRegistry(person);
+                notifyPhoneSystem();
+                sendWelcomeEmail(person);
+            } catch (Exception e) {
+                failedPeople.add(person);
+            }
+        }
+        
+        return failedPeople;
+    }
+
+    public boolean addPersonWithFailures(Person person) {
+        try {
+            placeInNameRegistry(person);
+            notifyPhoneSystem();
+            sendWelcomeEmail(person);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public boolean addPersonWithFailures2(Person person) {
+        try {
+            addPerson(person);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public void addPerson(Person person) {
         placeInNameRegistry(person);
         notifyPhoneSystem();
         sendWelcomeEmail(person);
-      } catch (Exception e) {
-        failedPeople.add(person);
-      }
     }
-    return failedPeople;
-  }
-  
-  public boolean addPersonWithFailures(Person person) {
-    try {
-      placeInNameRegistry(person);
-      notifyPhoneSystem();
-      sendWelcomeEmail(person);
-      return true;
-    } catch (Exception e) {
-      return false;
+
+    public Person parseName(String rawName) {
+        String[] namePieces = rawName.split(" ");
+        String firstName, lastName;
+
+        if (namePieces.length < 2)
+            throw new IllegalArgumentException("We must have 2 names");
+
+        firstName = namePieces[0];
+        lastName = namePieces[namePieces.length - 1];
+
+        return new Person(firstName, lastName);
     }
-  }
-  
-  
-  public boolean addPersonWithFailures2(Person person) {
-    try {
-      addPerson(person);
-      return true;
-    } catch (Exception e) {
-      return false;
+
+    public void removePerson(Person person) {
+        removeFromNameRegistry(person);
+        notifyPhoneSystem();
+        sendFarewellEmail(person);
     }
-  }
-  
-  public void addPerson(Person person) {
-    placeInNameRegistry(person);
-    notifyPhoneSystem();
-    sendWelcomeEmail(person);
-  }
 
-  public Person parseName(String rawName) {
-    String[] namePieces = rawName.split(" ");
-    String firstName, lastName;
+    private void sendFarewellEmail(Person person) {
+    }
 
-    if (namePieces.length < 2)
-      throw new IllegalArgumentException("We must have 2 names");
+    private void removeFromNameRegistry(Person person) {
+    }
 
-    firstName = namePieces[0];
-    lastName = namePieces[namePieces.length - 1];
+    private void sendWelcomeEmail(Person person) {
+    }
 
-    return new Person(firstName, lastName);
-  }
+    private void updatePhoneSystem() {
+    }
 
-  public void removePerson(Person person) {
-    removeFromNameRegistry(person);
-    notifyPhoneSystem();
-    sendFarewellEmail(person);
-  }
+    private void placeInNameRegistry(Person person) {
+    }
 
-  private void sendFarewellEmail(Person person) {
-  }
-
-  private void removeFromNameRegistry(Person person) {
-  }
-
-  private void sendWelcomeEmail(Person person) {
-  }
-
-  private void updatePhoneSystem() {
-  }
-
-  private void placeInNameRegistry(Person person) {
-  }
-
-  private void notifyPhoneSystem() {
-  }
+    private void notifyPhoneSystem() {
+    }
 }
